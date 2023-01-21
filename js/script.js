87b5b41b34022afdbc2cbc3bed4dde0653c4a6aa
Mateusz Piaszczyk <mateuszpiaszczyk91@gmail.com>
2023-01-21 20:12:29 +0100

    add new function and refactoring

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-console.log("Cześć developerze!");
-console.log("Jeśli tu juz jesteś to zyczę Ci dobrego dnia!! PS: coś mi niedziała kropka nad <Z> pewnie to kwestia jakiegoś ustawienia w programie lub złego przypisania, ale juz mi się nie chciało z tym grzebać, takze ten... praca domowa wykonana, więc trzymaj się! :D");
+{
+    const welcome = () => {
+        console.log("Cześć developerze, miej dobry dzień!😀");
+    }
+    const hiddenHeader = document.querySelector(".hiddenHeader");
+    const header = document.querySelector(".section__header");
 
-let hiddenHeader = document.querySelector(".hiddenHeader");
-let header = document.querySelector(".section__header");
+    hiddenHeader.addEventListener("click", () => {
+        header.remove();
+    });
 
-hiddenHeader.addEventListener("click", () => {
-    header.remove();
-});
+    const toggleTheme = () => {
+        const body = document.documentElement;
+        const themeName = document.querySelector(".js-themeName");
 
-let switchTheme = document.querySelector(".js-switchThemeButton");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-themeName");
-let table = document.querySelector(".table__row");
+        body.classList.toggle("body--darkTheme");
+        themeName.innerText = body.classList.contains("body--darkTheme") ? "tryb normalny" : "wysoki kontrast";
 
+    }
 
-switchTheme.addEventListener("click", () => {
-    body.classList.toggle("body--darkTheme");
-    table.classList.toggle("tableRowDark");
+    const init = () => {
+        const switchTheme = document.querySelector(".js-switchThemeButton");
+        switchTheme.addEventListener("click", toggleTheme);
 
-    themeName.innerText = body.classList.contains("body--darkTheme") && table.classList.contains("tableRowDark") ? "tryb normalny" : "wysoki kontrast";
-
-});
-
-const numbers = [5, 10, 15, 20, 25, 30];
-const [number1, , number3, ...otherNumbers] = numbers;
+        welcome();
+    }
+    init ();
+}
